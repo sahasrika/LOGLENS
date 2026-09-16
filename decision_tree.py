7774504270e6c8160362082dbeb19ba3eb4f1762
sahasrika <sahasrikagowrishetty@gmail.com>
2026-09-16 16:57:54 +0530

    Keep only LogLens project in repository

--- decision_tree.py
+++
@@ -1,9 +0,0 @@
-from sklearn import tree
-from sklearn.datasets import load_iris
-
-iris = load_iris()
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(iris.data, iris.target)
-
-pred = clf.predict([[5.1, 3.5, 1.4, 0.2]])
-print(f"Predicted class: {iris.target_names[pred][0]}")
